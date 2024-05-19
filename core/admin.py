125052bf5125bef990afe3d659cf575fc2b3b86a
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-05-19 17:57:38 +0530

    created some modules Subscribe, Reviews and Faqs

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Subscribe, Reviews, Faqs
 
-# Register your models here.
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at', 'unsubscribed')
+    search_fields = ('email',)
+    list_filter = ('subscribed_at', 'unsubscribed')
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'content', 'rating', 'created_at')
+    search_fields = ('name', 'content')
+    list_filter = ('rating', 'created_at')
+
+@admin.register(Faqs)
+class FaqsAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'created_at', 'updated_at')
+    search_fields = ('question', 'answer')
+    list_filter = ('created_at', 'updated_at')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Subscribe(models.Model):
+    email = models.EmailField(max_length=254, unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    unsubscribed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+class Reviews(models.Model):
+    name = models.CharField(max_length=50)
+    content = models.CharField(max_length=350)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Faqs(models.Model):
+    question = models.CharField(max_length=250)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
